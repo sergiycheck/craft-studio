187a004ebbe24d3bb9ca5fadae51c5d7361f1a76
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-01-01 17:48:00 +0200

    feat: svgs, team members

--- components/team-members.tsx
+++ components/team-members.tsx
@@ -1,51 +1,43 @@
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
+import { LinkedInSvg, WrappedSvgLink } from "./common/svgs";
+import Link from "next/link";
 
-import TestimonialImage01 from "@/public/images/testimonial-01.jpg";
-import TestimonialImage02 from "@/public/images/testimonial-02.jpg";
-import TestimonialImage03 from "@/public/images/testimonial-03.jpg";
-
-const testimonials = [
+const teamMembers = [
   {
-    image: TestimonialImage01,
+    image: "https://loremflickr.com/640/480/people",
     name: "Anastasia Dan",
-    title: "UX Board",
-    children:
-      "Open PRO lets me quickly get the insights I care about so that I can focus on my productive work. I've had Open PRO for about 24 hours now and I honestly don't know how I functioned without it before.",
+    title: "Frontend Developer",
   },
   {
-    image: TestimonialImage02,
+    image: "https://loremflickr.com/640/480/people",
     name: "Anastasia Dan",
-    title: "UX Board",
-    children:
-      "Open PRO lets me quickly get the insights I care about so that I can focus on my productive work. I've had Open PRO for about 24 hours now and I honestly don't know how I functioned without it before.",
+    title: "Backend Developer",
   },
   {
-    image: TestimonialImage03,
+    image: "https://loremflickr.com/640/480/people",
     name: "Anastasia Dan",
-    title: "UX Board",
-    children:
-      "Open PRO lets me quickly get the insights I care about so that I can focus on my productive work. I've had Open PRO for about 24 hours now and I honestly don't know how I functioned without it before.",
+    title: "UX Designer",
   },
 ];
 
 export default function TeamMembers() {
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="max-screen-2xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20 border-t border-gray-800">
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Don't take our word for it</h2>
+          <div className="pb-12 md:pb-20">
+            <h2 className="h2 mb-4">Meet out team members</h2>
+
             <p className="text-xl text-gray-400">
-              Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam
-              ut venenatis tellus—in ornare.
+              Meet Craft Studio team members. We are a small team of designers and developers working together to create
+              beautiful, easy to use solutions for the world.
             </p>
           </div>
 
-          {/* Testimonials */}
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
-            {testimonials.map((testimonial, index) => (
-              <Testimonial key={index} {...testimonial} />
+            {teamMembers.map((item, index) => (
+              <TeamMember key={index} {...item} />
             ))}
           </div>
         </div>
@@ -54,34 +46,33 @@ export default function TeamMembers() {
   );
 }
 
-type TestimonialProps = {
-  image: StaticImageData;
+type TeamMemberProps = {
+  image: string;
   name: string;
   title: string;
-  children: React.ReactNode;
 };
 
-function Testimonial({ image, name, title, children }: TestimonialProps) {
+function TeamMember({ image, name, title }: TeamMemberProps) {
   return (
-    <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
-      <div>
-        <div className="relative inline-flex flex-col mb-4">
-          <Image className="rounded-full" src={image} width={48} height={48} alt={name} />
-          <svg
-            className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
-            viewBox="0 0 24 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-          </svg>
+    <div className="flex flex-col h-full  border border-gray-700" data-aos="fade-up">
+      <div className="flex justify-center p-6">
+        <div className="w-[200px] h-[200px] relative">
+          <Image className="rounded-full w-full h-full" fill objectFit="cover" src={image} alt={name} />
         </div>
       </div>
-      <blockquote className="text-lg text-gray-400 grow">{children}</blockquote>
-      <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-        <cite className="text-gray-200 not-italic">{name}</cite> -{" "}
-        <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">
-          {title}
-        </a>
+      <div className="flex flex-col text-center justify-center p-6 mt-6  font-medium">
+        <p className="text-lg text-gray-200 hover:text-gray-100 transition duration-150 ease-in-out">{name}</p>
+        <p className="text-md text-gray-400">{title}</p>
+      </div>
+
+      <div className="flex flex-col items-center p-6 mt-6 border-t border-gray-700 font-medium">
+        <Link
+          className="w-fit text-white border border-gray-700 hover:scale-150 transition duration-150 ease-in-out  "
+          href="#"
+          aria-label="LinkedIn"
+        >
+          <LinkedInSvg />
+        </Link>
       </div>
     </div>
   );
