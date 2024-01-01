756bc87a4354bd2eb1e9d6608d3dc2cccd3e7adf
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-01-01 19:43:18 +0200

    feat: add aos to team members block

--- components/team-members.tsx
+++ components/team-members.tsx
@@ -23,11 +23,11 @@ const teamMembers = [
 export default function TeamMembers() {
   return (
     <section>
-      <div className="max-screen-2xl mx-auto px-4 sm:px-6">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20 border-t border-gray-800">
           {/* Section header */}
           <div className="pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Meet out team members</h2>
+            <h2 className="h2 mb-4">Our Team</h2>
 
             <p className="text-xl text-gray-400">
               Meet Craft Studio team members. We are a small team of designers and developers working together to create
@@ -35,9 +35,13 @@ export default function TeamMembers() {
             </p>
           </div>
 
-          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+          <div
+            data-aos-team-member-blocks
+            className="max-w-sm mx-auto grid gap-8 
+              lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none"
+          >
             {teamMembers.map((item, index) => (
-              <TeamMember key={index} {...item} />
+              <TeamMember key={index} {...item} delay={index * 100} />
             ))}
           </div>
         </div>
@@ -50,11 +54,18 @@ type TeamMemberProps = {
   image: string;
   name: string;
   title: string;
+  delay: number;
 };
 
-function TeamMember({ image, name, title }: TeamMemberProps) {
+function TeamMember({ image, name, title, delay }: TeamMemberProps) {
   return (
-    <div className="flex flex-col h-full  border border-gray-700" data-aos="fade-up">
+    <div
+      className="flex flex-col h-full 
+      border border-gray-700"
+      data-aos="fade-up"
+      data-aos-anchor="[data-aos-team-member-blocks]"
+      data-aos-delay={delay}
+    >
       <div className="flex justify-center p-6">
         <div className="w-[200px] h-[200px] relative">
           <Image className="rounded-full w-full h-full" fill objectFit="cover" src={image} alt={name} />
@@ -67,7 +78,7 @@ function TeamMember({ image, name, title }: TeamMemberProps) {
 
       <div className="flex flex-col items-center p-6 mt-6 border-t border-gray-700 font-medium">
         <Link
-          className="w-fit text-white border border-gray-700 hover:scale-150 transition duration-150 ease-in-out  "
+          className="w-fit text-white border border-gray-700 hover:scale-110 transition duration-150 ease-in-out  "
           href="#"
           aria-label="LinkedIn"
         >
